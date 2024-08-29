e432ac9e94d77db2e0eee6f69fbf53c1b2dec10e
unknown <kurniantolucky9@gmail.com>
2024-08-29 18:24:55 +0700

    first commit

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import MoviesCard from "../Card/MoviesCard";
+
+const Movies = ({listPopularMovie}) => {
+  return (
+    <div className="bg-white px-4 py-8">
+      <h1 className="text-xl font-bold mb-4">Recent Movies</h1>
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+        {listPopularMovie.map((movie, i)=>(
+          <MoviesCard key={i} title={movie.title} date={movie.release_date} poster={movie.poster_path} vote_average={movie.vote_average} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Movies;
